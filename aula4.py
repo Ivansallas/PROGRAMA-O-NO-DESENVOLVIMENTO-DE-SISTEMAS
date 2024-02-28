5985f13c2a6f33333787128f127c2ae9335c3ed7
kiss_ivan@hotmail.com <79423340+Ivansallas@users.noreply.github.com>
2024-02-27 22:07:40 -0300

    aula4

---
+++ aula4.py
@@ -0,0 +1,28 @@
+# LAÇO FOR
+# Itera sobre uma sequência (como uma lista, string) ou qualquer objeto iterável.
+# Iterando sobre uma lista
+lista = [1, 2, 3, 4, 5]
+for elemento in lista:
+    print(elemento)
+
+"""# Iterando sobre uma string
+texto = "Python"
+for caractere in texto:
+    print(caractere)
+
+# Usando a função range() A função range é uma função incorporada na linguagem de programação Python que é usada para gerar uma sequência de números
+for i in range(50):
+    print(i)
+
+#LAÇO WHILE
+#Executa repetidamente um bloco de código enquanto uma condição é verdadeira.
+# Imprimindo números de 0 a 4 usando while
+contador = 0
+while contador < 5:
+    print(contador)
+    contador += 1
+
+# Pedindo entrada até que o usuário entre com um valor válido
+entrada = None
+while entrada != "correto":
+    entrada = input("Digite 'correto' para continuar: ")"""
